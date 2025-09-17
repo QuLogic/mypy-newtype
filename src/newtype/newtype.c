0d12c5f617289a7a36f56430405f225f6f366bb4
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2025-09-17 16:36:05 -0400

    Add FooType alias to the extension as well

--- src/newtype/newtype.c
+++ src/newtype/newtype.c
@@ -11,6 +11,13 @@ newtype_foo(PyObject *self, PyObject *obj)
     return obj;
 }
 
+static int
+newtype_mod_exec(PyObject *module)
+{
+    PyObject *int_obj = (PyObject *)&PyLong_Type;
+    return PyModule_AddObjectRef(module, "FooType", int_obj);
+}
+
 static PyMethodDef newtype_methods[] = {
     {
         .ml_name = "foo",
@@ -20,10 +27,16 @@ static PyMethodDef newtype_methods[] = {
     { NULL, NULL, 0, NULL }
 };
 
+static PyModuleDef_Slot newtype_slots[] = {
+    {Py_mod_exec, &newtype_mod_exec},
+    { 0, NULL }
+};
+
 static struct PyModuleDef newtype_module = {
     .m_base = PyModuleDef_HEAD_INIT,
     .m_name = "newtype",
     .m_methods = newtype_methods,
+    .m_slots = newtype_slots,
 };
 
 PyMODINIT_FUNC
